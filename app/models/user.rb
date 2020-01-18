5927ead9a09eefbc6c0d8e8da54bd10c3c758d42
tippy <tippy@tom.sfc.keio.ac.jp>
2020-01-19 02:02:01 +0900

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+
+  has_secure_password
+  validates :name, {presence: true, format: {with: /\A[a-zA-Z0-9]+\z/ }, length: {maximum: 16} }
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, maximum: 24 }
+
+end
