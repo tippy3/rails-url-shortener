35b436adebeb43daeb2128be878aa69a5662bcf4
tippy <tippy@tom.sfc.keio.ac.jp>
2020-01-19 02:14:17 +0900

    add user controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,13 @@
+class UserController < ApplicationController
+  def signup
+  end
+
+  def login
+  end
+
+  def mypage
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'user/signup'
+  get 'user/login'
+  get 'user/mypage'
+  get 'user/edit'
   get 'home/index'
   get 'home/privacy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
